9b5f0a799b37e2b3e90d6b5aa566e7c251db7666
Colby Caplan <128941274+colbycaplan@users.noreply.github.com>
2025-07-29 16:48:41 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,14 @@
-const CACHE_NAME = 'animal-car-game-v6';
+const CACHE_NAME = 'animal-car-game-v7';
 const urlsToCache = [
     './',
     './index.html',
     './manifest.json',
     'https://cdn.tailwindcss.com',
     'https://fonts.googleapis.com/css2?family=Fredoka+One&family=Poppins:wght@400;600;700&display=swap',
-    'https://placehold.co/192x192/A16207/FEF3C7?text=ACG',
-    'https://placehold.co/512x512/A16207/FEF3C7?text=ACG'
+    './images/icon-512.png',
+    './images/icon-1024.png',
+    './images/screenshot1.png',
+    './images/screenshot2.png'
 ];
 
 self.addEventListener('install', event => {
